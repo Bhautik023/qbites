c7bff04c61c793a09f1925e263285082e32c6ea5
Bhautik Dudhat <168476578+Bhautik023@users.noreply.github.com>
2025-09-01 23:44:32 +0530

    ../

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/proxy/brochures/:path*",
+                destination:
+                    "https://srv733641.hstgr.cloud:22443/brochures/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
